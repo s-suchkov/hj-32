15cb266840d1b4b675c76e50c82accb9fdfa9ed4
Sergey <ser3421@gmail.com>
2019-06-22 00:47:12 +0400

    sec

--- js/diplom.js
+++ js/diplom.js
@@ -1,4 +1,12 @@
 'use strict'
 const comments = document.querySelector('.comments')
 comments.style.display = 'none'
-console.log('gjd')
+const draw = document.querySelector('.draw')
+draw.style.display = 'none'
+const share = document.querySelector('.share')
+share.style.display = 'none'
+const new = document.querySelector('.new')
+new.addEventListener('change', (event) => {
+    let file = event.currentTarget.files
+    console.log(file)
+})
