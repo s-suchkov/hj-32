1b37440c65065779e29fb1b77b253be9d080c736
Sergey <ser3421@gmail.com>
2019-06-22 15:05:48 +0400

    sec

--- js/diplom.js
+++ js/diplom.js
@@ -6,6 +6,8 @@ draw.style.display = 'none'
 const share = document.querySelector('.share')
 share.style.display = 'none'
 const newLoad = document.querySelector('.new')
+newLoad.type = 'file'
+newLoad.accept = 'image/jpeg, image/png'
 newLoad.addEventListener('change', (event) => {
     let file = event.currentTarget.files
     console.log(file)
